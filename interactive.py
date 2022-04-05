e92bce1cb2402f0d201b8a2a8568aec77df48ec4
Jason Roman <funjasonroman@gmail.com>
2022-04-04 19:13:56 -0600

    Added strict mode for markov chain
    Also added shortcut key Ctrl-Z in interactive mode: Finds a new suggestion.

--- interactive.py
+++ interactive.py
@@ -1,13 +1,13 @@
 from getch import getch
 from sys import stdout
-from markov import WordMarkovChain, MarkovChain
+from markov import *
 import os
 
 def addSuggestion(s):
-	print(u"\u001b[30;1m" + s.replace("\n", "↲").replace("\t", "↦") + u"\u001b[0m\u001b[" + str(len(s)) + "D", end="")
+	print(u"\u001b[34m" + s.replace("\n", "↲").replace("\t", "↦") + u"\u001b[0m\u001b[" + str(len(s)) + "D", end="")
 
 # Train the Markov chain.
-x = (WordMarkovChain if "WORD" in os.environ and os.environ["WORD"] else MarkovChain)()
+x = (WordMarkovChain if "WORD" in os.environ and os.environ["WORD"] else MarkovChain)("STRICT" in os.environ and os.environ["STRICT"])
 files = os.listdir("test_files")
 for filename in files:
 	f = open("test_files/" + filename, "r")
@@ -23,6 +23,7 @@ while True:
 		print(u"\r\u001b[2K\u001b[1A", end="")
 	print("\r\u001b[2K", end="")
 	# If the user pressed a key, add it to the text.
+	newSuggestion = True
 	if newChar == '\x7f':
 		# Backspace
 		text = text[:-1]
@@ -30,6 +31,17 @@ while True:
 		# Ctrl-C
 		print(text)
 		exit()
+	elif newChar == '\x1a':
+		# Ctrl-Z
+		# Generate a new suggestion.
+		prev = suggestion
+		tries = 0
+		x.strict = False
+		while suggestion == prev and tries < 10000:
+			suggestion = x.generateFromPrevious(text)
+			tries += 1
+		x.strict = ("STRICT" in os.environ and os.environ["STRICT"])
+		newSuggestion = False # so we can generate our own new suggestion.
 	elif newChar == '\t':
 		# Tab
 		text += suggestion
@@ -46,10 +58,13 @@ while True:
 	else:
 		# Normal character
 		text += newChar
-	# Print what they have typed so far.
-	print(text, end="")
+	# Print what they have typed so far, highlighting last word.
+	if len(words(text)):
+		w = len(words(text)[-1])
+		print(text[:-w], end=u"\u001b[31m")
+		print(text[-w:], end=u"\u001b[0m")
 	# Find a new suggestion.
-	suggestion = x.generateFromPrevious(text)
+	if newSuggestion: suggestion = x.generateFromPrevious(text)
 	# Print the suggestion.
 	if suggestion:
 		addSuggestion(suggestion)
--- markov.py
+++ markov.py
@@ -1,9 +1,11 @@
 from random import choice
+from statistics import mode # Using mode() to get the most common value in a list, for strict mode
 
 class MarkovChain:
-	def __init__(self):
+	def __init__(self, strict: bool = False):
 		self.dir: dict[str, list[str]] = {}
 		self.start: list[str] = []
+		self.strict = strict
 	def recordSingle(self, before, after):
 		if before not in self.dir: self.dir[before] = []
 		self.dir[before].append(after)
@@ -15,7 +17,8 @@ class MarkovChain:
 	def predictNextChar(self, before):
 		if before not in self.dir: return "";
 		if len(self.dir[before]) == 0: return "";
-		return choice(self.dir[before])
+		if self.strict: return mode(self.dir[before])
+		else: return choice(self.dir[before])
 	def generateFromPrevious(self, before):
 		if len(before) == 0: return "";
 		last = before[-1]
