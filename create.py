a91ac1f6a177d2417db0ce3c419213b77f562761
Jason Roman <funjasonroman@gmail.com>
2022-04-05 17:39:27 -0600

    create.py uses selector script. Also added main.py

--- create.py
+++ create.py
@@ -1,7 +1,21 @@
 import markov
 import os
+import selector
 
-x = (markov.WordMarkovChain if "WORD" in os.environ and os.environ["WORD"] else markov.MarkovChain)()
+running = True
+while running:
+	choice = selector.selector("Create File", ["", "Character Mode >", "Word Mode >"])
+	if choice == 1:
+		running = False
+		x = markov.MarkovChain()
+	elif choice == 2:
+		running = False
+		x = markov.WordMarkovChain()
+	elif choice == -1:
+		exit()
+
+g = selector.progress("Generating file...")
+g.send(None)
 
 files = os.listdir("test_files")
 for filename in files:
@@ -9,8 +23,12 @@ for filename in files:
 	x.trainWith(f.read())
 	f.close()
 
-t = x.beginGeneratingText().predict(5000 if "WORD" in os.environ and os.environ["WORD"] else 20000)
+iters = (5000 if isinstance(x, markov.WordMarkovChain) else 20000)
+t = x.beginGeneratingText()
+for i in range(iters):
+	t.predict(1)
+	g.send(f"Generating file... ({round((i / iters) * 100)}% done)")
 
 f = open("orig.py", "w")
-f.write(t)
+f.write(t.value)
 f.close()
